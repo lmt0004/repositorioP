30ad477ae70aa7899bd22b06f0e89e304edd77f5
lmt0004 <133787061+lmt0004@users.noreply.github.com>
2023-05-29 11:39:55 +0200

    Add files via upload

---
+++ EjercicioClasePOO/src/ejercicioclasepoo/Coche.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicioclasepoo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Coche {
+    private String marca, modelo, matricula;
+    private double precio;
+    static double descuento = 1500;
+    
+}
---
+++ EjercicioClasePOO/src/ejercicioclasepoo/EjercicioClasePOO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioclasepoo;
+
+/**
+ *
+ * @author usuario
+ */
+public class EjercicioClasePOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Coche hyundaii30 = new Coche();
+        System.out.println("El descuento es: " + Coche.descuento);
+    }
+    
+}
